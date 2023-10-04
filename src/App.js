6724c503853a5f3b4804b7e7c0bed7539b58a006
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-10-04 21:50:15 +0200

    create index.css and component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 
+function Header() {
+	return (
+		<>
+			<h1>React Project Menu</h1>
+		</>
+	);
+}
+
+function Main() {
+	return <Pizza />;
+}
+
+function Footer() {
+	return (
+		<footer>
+			{new Date().toLocaleTimeString()} .We'are open at this moment{" "}
+		</footer>
+	);
+}
+
+function Pizza() {
+	return (
+		<>
+			<h3>Pizza Name</h3>
+			<p>loreem loreemloreemloreemloreemloreemloreemloreem loreem loreem </p>
+			<img src='pizzas/focaccia.jpg' alt='pizza' />
+		</>
+	);
+}
 export default App;
