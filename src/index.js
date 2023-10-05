d2e27ad03f9eef24cfb27d7a79cd8f8283fc53e1
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-10-05 20:42:21 +0200

    add new component and index.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,90 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+	{
+		name: "Focaccia",
+		ingredients: "Bread with italian olive oil and rosemary",
+		price: 6,
+		photoName: "pizzas/focaccia.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Margherita",
+		ingredients: "Tomato and mozarella",
+		price: 10,
+		photoName: "pizzas/margherita.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Spinaci",
+		ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+		price: 12,
+		photoName: "pizzas/spinaci.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Funghi",
+		ingredients: "Tomato, mozarella, mushrooms, and onion",
+		price: 12,
+		photoName: "pizzas/funghi.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Salamino",
+		ingredients: "Tomato, mozarella, and pepperoni",
+		price: 15,
+		photoName: "pizzas/salamino.jpg",
+		soldOut: true,
+	},
+	{
+		name: "Pizza Prosciutto",
+		ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+		price: 18,
+		photoName: "pizzas/prosciutto.jpg",
+		soldOut: false,
+	},
+];
+
+function Header() {
+	return (
+		<>
+			<h1>Menu Pizza</h1>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industry's standard dummy text ever
+				since the 1500s, when an unknown printer took a galley of type and
+				scrambled it to make a type specimen book. It has survived not only five
+				centuries, but also the leap into electronic typesetting, remaining
+				essentially unchanged. It was popularised in the 1960s with the release
+				of Letraset sheets containing Lorem Ipsum passages, and more recently
+				with desktop publishing software like Aldus PageMaker including versions
+				of Lorem Ipsum.
+			</p>
+		</>
+	);
+}
+
+function Main() {
+	return (
+		<>
+			<h2>title</h2>
+			<p>lor</p>
+			<img src='pizzas/focaccia.jpg' alt='pizza' />
+		</>
+	);
+}
+
+function Footer() {}
 function App() {
-  return <h1>Hello World</h1>;
+	return (
+		<>
+			<Header />
+			<Main />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
