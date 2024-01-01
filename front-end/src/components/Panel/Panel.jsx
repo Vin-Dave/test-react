21af4e0c2b8f7b7e841fb7d2d2bffa1b0540dabe
Vin-Dave <vin.dave.9702@gmail.com>
2024-01-01 21:37:57 +0100

    update project

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button/Button";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+  const [data, setData] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
-    return (
-        <>
-            <Button>Załaduje dane</Button>
-            <section className={styles.section}>
-                <List data={data}></List>
-            </section>
-        </>
-    );
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then((r) => r.json())
+      .then((d) => {
+        setData(d);
+        setisLoading(false);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  if (isLoading) return <p>Loading</p>;
+  return (
+    <>
+      <Button>Załaduje dane</Button>
+      <section className={styles.section}>
+        <List data={data}></List>
+      </section>
+    </>
+  );
 }
